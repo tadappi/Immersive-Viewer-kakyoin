728b8a1e710fa447d8b3baab933cd4a78205eb27
tadappi <iskyimaging@gmail.com>
2026-05-03 12:20:52 +0900

    add

---
+++ data.js
@@ -0,0 +1,59 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kakyouin",
+      "name": "kakyouin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 1.602859,
+        "pitch": 1.562257,
+        "fov": 1.3263317910021355
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.957542,
+          "pitch": 0.197450,
+          "title": "仙台駅",
+          "text": ""
+        },
+        {
+          "yaw": -1.663748,
+          "pitch": 0.155261,
+          "title": "AERショッピングセンター",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
